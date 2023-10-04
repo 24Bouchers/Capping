224d648e945518d39325722a135594d5826dc44e
Steven J Boucher <Steven.boucher1@marist.edu>
2023-10-04 12:58:38 -0400

    Api Update (Buiding Tables)

--- Backend/appy.py
+++
@@ -1,9 +0,0 @@
-#python -m pip install flask
-
-# I'm pretty Sure Flask is how we will communitcate with the website
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello, Flask!"
