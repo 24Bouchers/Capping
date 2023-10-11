aee86d29d7a5e50006ea0875e88c75b53ebfb2f1
Steven J Boucher <70175139+24Bouchers@users.noreply.github.com>
2023-10-10 21:41:26 -0400

    Steve b (#5)
    * Ip Connection
    
    * ReOrg
    
    * DB + LiamDisplay Update

---
+++ Web Interface/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, app, render_template, request
+import mariadb
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def display_data():
+    conn = mariadb.connect(host='10.10.9.43', user='root', password='', db='ArchFiber')
+    cursor = conn.cursor(mariadb.cursors.DictCursor)
+    
+    query = request.form.get('query') if request.method == 'POST' else None
+    
+    if query:
+        # Query is searching from database based on name, mac address, ipv4 or ipv6 address
+        # It is NOT case sensitive
+        # Used Parameterized quieries to protect from SQL injections
+        cursor.execute('''SELECT * FROM Data 
+                       WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''', 
+                       ('%' + query + '%', '%' + query + '%', '%' + query + '%', '%' + query + '%'))
+    else:
+        cursor.execute('SELECT * FROM customers;')
+
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return render_template('table_display.html', rows=rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
