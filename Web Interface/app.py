804ca1924c49b1937d84ddb9c76d62366cee3b0d
Steven J Boucher <Steven.boucher1@marist.edu>
2023-10-12 14:25:10 -0400

    Fix Import

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -1,67 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from flask import Flask, app, flash, redirect, render_template, request
-import mariadb
-
-app = Flask(__name__)
-app.secret_key = 'ArchFiber23'
-
-
-@app.route('/')
-def main():
-    return render_template('index.html')
-
-
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/addDevice.html', methods=['GET', 'POST'])
-def addDevice():
-    if request.method == 'POST':
-        # Get data from the form
-        name = request.form['customer']
-        mac_address = request.form['mac_address']
-        ipv4_address = request.form['ipv4_address']
-        ipv6_address = request.form['ipv6_address']
-
-        # Connect to MariaDB
-        conn = mariadb.connect(
-            host='10.10.9.43', user='root', password='', db='customer_data')
-        cursor = conn.cursor()
-
-        # Insert the device data into the database
-        sql = '''INSERT INTO customers (name, mac_address, ipv4_address, ipv6_address) 
-                 VALUES (%s, %s, %s, %s)'''
-        cursor.execute(sql, (name, mac_address, ipv4_address, ipv6_address))
-        conn.commit()
-
-        cursor.close()
-        conn.close()
-
-        flash('Device added successfully!')
-        return redirect('/devices.html')
-
-    return render_template('addDevice.html')
-
-
-@app.route('/devices.html', methods=['GET', 'POST'])
-def devices():
-    conn = mariadb.connect(host='10.10.9.43', user='root',
-                           password='', db='customer_data')
-    cursor = conn.cursor(mariadb.cursors.DictCursor)
-
-    query = request.form.get('query') if request.method == 'POST' else None
-
-=======
-from flask import Flask, app, render_template, request
-import mariadb
-=======
 import subprocess
 from flask import Flask, app, flash, redirect, render_template, request
 import pymysql
->>>>>>> f1f21b6 (Reload from Liam branch appy is now TableInit.py)
 
 app = Flask(__name__)
 app.secret_key = 'ArchFiber23'
@@ -73,9 +12,9 @@ def main():
 
 def ping_ipv4():
     # Retrieve IP addresses from MariaDB
-    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+    conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='customer_data')
     cursor = conn.cursor()
-    cursor.execute('SELECT ipv4_address FROM customers;')
+    cursor.execute('SELECT ipv4_address FROM radacct;')
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
@@ -106,7 +45,7 @@ def addDevice():
         ipv6_address = request.form['ipv6_address']
 
         # Connect to MariaDB
-        conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+        conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='radacct')
         cursor = conn.cursor()
 
         # Insert the device data into the database
@@ -125,27 +64,17 @@ def addDevice():
 
 @app.route('/devices.html', methods=['GET', 'POST'])
 def devices():
-    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+    conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='radacct')
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     
     query = request.form.get('query') if request.method == 'POST' else None
     
->>>>>>> 4aedf9e (Steve b (#5))
     if query:
         # Query is searching from database based on name, mac address, ipv4 or ipv6 address
         # It is NOT case sensitive
         # Used Parameterized quieries to protect from SQL injections
-<<<<<<< HEAD
-<<<<<<< HEAD
         cursor.execute('''SELECT * FROM customers 
-                       WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''',
-=======
-        cursor.execute('''SELECT * FROM Data 
-=======
-        cursor.execute('''SELECT * FROM customers 
->>>>>>> f1f21b6 (Reload from Liam branch appy is now TableInit.py)
                        WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''', 
->>>>>>> 4aedf9e (Steve b (#5))
                        ('%' + query + '%', '%' + query + '%', '%' + query + '%', '%' + query + '%'))
     else:
         cursor.execute('SELECT * FROM customers;')
@@ -154,16 +83,7 @@ def devices():
     cursor.close()
     conn.close()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     return render_template('devices.html', rows=rows)
-
-=======
-    return render_template('table_display.html', rows=rows)
->>>>>>> 4aedf9e (Steve b (#5))
-=======
-    return render_template('devices.html', rows=rows)
->>>>>>> f1f21b6 (Reload from Liam branch appy is now TableInit.py)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Web Interface/display-devices.py
+++ Web Interface/display-devices.py
@@ -5,21 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_data():
-<<<<<<< HEAD
-<<<<<<< HEAD
-    conn = mariadb.connect(host='10.10.9.43', user='root', password='', db='customer_data')
-    cursor = conn.cursor(mariadb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM customers;')
-=======
-    conn = mariadb.connect(host='10.10.9.43', user='root', password='', db='ArchFiber')
-    cursor = conn.cursor(mariadb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM Data;')
->>>>>>> 4aedf9e (Steve b (#5))
-=======
-    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+    conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='customer_data')
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    cursor.execute('SELECT * FROM customers;')
->>>>>>> f1f21b6 (Reload from Liam branch appy is now TableInit.py)
+    cursor.execute('SELECT * FROM radacct;')
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
