41a7901fa42d22f569baf00cb543e1c85ae2c751
Liam Haggerty <70300038+LiamHaggerty1@users.noreply.github.com>
2023-10-15 22:39:09 +0100

    Update app.py (#7)

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -1,12 +1,45 @@
-from flask import Flask, app, render_template, request
-import mariadb
+from flask import Flask, app, flash, redirect, render_template, request
+import pymysql
 
 app = Flask(__name__)
+app.secret_key = 'ArchFiber23'
 
-@app.route('/', methods=['GET', 'POST'])
-def display_data():
-    conn = mariadb.connect(host='10.10.9.43', user='root', password='', db='ArchFiber')
-    cursor = conn.cursor(mariadb.cursors.DictCursor)
+@app.route('/')
+@app.route('/index.html')
+def main():
+    return render_template('index.html')
+
+@app.route('/addDevice.html', methods=['GET', 'POST'])
+def addDevice():
+    if request.method == 'POST':
+        # Get data from the form
+        name = request.form['customer']
+        mac_address = request.form['mac_address']
+        ipv4_address = request.form['ipv4_address']
+        ipv6_address = request.form['ipv6_address']
+
+        # Connect to MariaDB
+        conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+        cursor = conn.cursor()
+
+        # Insert the device data into the database
+        sql = '''INSERT INTO customers (name, mac_address, ipv4_address, ipv6_address) 
+                 VALUES (%s, %s, %s, %s)'''
+        cursor.execute(sql, (name, mac_address, ipv4_address, ipv6_address))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        flash('Device added successfully!')
+        return redirect('/devices.html')
+
+    return render_template('addDevice.html')
+
+@app.route('/devices.html', methods=['GET', 'POST'])
+def devices():
+    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
     
     query = request.form.get('query') if request.method == 'POST' else None
     
@@ -14,7 +47,7 @@ def display_data():
         # Query is searching from database based on name, mac address, ipv4 or ipv6 address
         # It is NOT case sensitive
         # Used Parameterized quieries to protect from SQL injections
-        cursor.execute('''SELECT * FROM Data 
+        cursor.execute('''SELECT * FROM customers 
                        WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''', 
                        ('%' + query + '%', '%' + query + '%', '%' + query + '%', '%' + query + '%'))
     else:
@@ -24,7 +57,7 @@ def display_data():
     cursor.close()
     conn.close()
 
-    return render_template('table_display.html', rows=rows)
+    return render_template('devices.html', rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
