ca4f06fa497429c2bab4b091ab30412dcadb5b15
Steven J Boucher <Steven.boucher1@marist.edu>
2023-10-16 14:55:46 -0400

    Merge Fix

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -1,4 +1,3 @@
-import subprocess
 from flask import Flask, app, flash, redirect, render_template, request
 import pymysql
 
@@ -10,46 +9,21 @@ app.secret_key = 'ArchFiber23'
 def main():
     return render_template('index.html')
 
-def ping_ipv4():
-    # Retrieve IP addresses from MariaDB
-    conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='customer_data')
-    cursor = conn.cursor()
-    cursor.execute('SELECT ipv4_address FROM radacct;')
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    
-    # Ping IP addresses and count successes/failures
-    successful_pings = 0
-    failed_pings = 0
-    
-    for row in rows:
-        ip = row[0]
-        response = subprocess.run(['ping', '-c', '1', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        if response.returncode == 0:
-            successful_pings += 1
-        else:
-            failed_pings += 1
-    
-    # 3. Display results
-    return render_template('index.html', successful_pings=successful_pings, failed_pings=failed_pings)
-
 @app.route('/addDevice.html', methods=['GET', 'POST'])
 def addDevice():
     if request.method == 'POST':
         # Get data from the form
-        name = request.form['radacct']
-        mac_address = request.form['callingstationid']
-        ipv4_address = request.form['framedipaddress']
-        ipv6_address = request.form['framedipv6address']
+        name = request.form['customer']
+        mac_address = request.form['mac_address']
+        ipv4_address = request.form['ipv4_address']
+        ipv6_address = request.form['ipv6_address']
 
         # Connect to MariaDB
-        conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='customer_data')
+        conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
         cursor = conn.cursor()
 
         # Insert the device data into the database
-        sql = '''INSERT INTO radacct (name, mac_address, ipv4_address, ipv6_address) 
+        sql = '''INSERT INTO customers (name, mac_address, ipv4_address, ipv6_address) 
                  VALUES (%s, %s, %s, %s)'''
         cursor.execute(sql, (name, mac_address, ipv4_address, ipv6_address))
         conn.commit()
@@ -64,7 +38,7 @@ def addDevice():
 
 @app.route('/devices.html', methods=['GET', 'POST'])
 def devices():
-    conn = pymysql.connect(host='localhost', user='root', password='ArchFiber23', db='customer_data')
+    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     
     query = request.form.get('query') if request.method == 'POST' else None
@@ -73,11 +47,11 @@ def devices():
         # Query is searching from database based on name, mac address, ipv4 or ipv6 address
         # It is NOT case sensitive
         # Used Parameterized quieries to protect from SQL injections
-        cursor.execute('''SELECT * FROM radacct 
-                       WHERE name LIKE %s or callingstationid LIKE %s or framedipaddress LIKE %s or framedipv6address LIKE %s;''', 
+        cursor.execute('''SELECT * FROM customers 
+                       WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''', 
                        ('%' + query + '%', '%' + query + '%', '%' + query + '%', '%' + query + '%'))
     else:
-        cursor.execute('SELECT * FROM radacct;')
+        cursor.execute('SELECT * FROM customers;')
 
     rows = cursor.fetchall()
     cursor.close()
