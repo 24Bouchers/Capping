8aa73d31bdb4070bd750b7a915c0769043110131
Steven J Boucher <Steven.boucher1@marist.edu>
2023-10-17 15:46:36 -0400

    Db value change

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -12,18 +12,18 @@ def main():
 @app.route('/addDevice.html', methods=['GET', 'POST'])
 def addDevice():
     if request.method == 'POST':
-        # Get data from the form
-        name = request.form['customer']
-        mac_address = request.form['mac_address']
-        ipv4_address = request.form['ipv4_address']
-        ipv6_address = request.form['ipv6_address']
+        # Get data from the form with default values
+        name = request.form.get('name', default=None)
+        mac_address = request.form.get('mac_address', default=None)
+        ipv4_address = request.form.get('ipv4_address', default=None)
+        ipv6_address = request.form.get('ipv6_address', default=None)
 
         # Connect to MariaDB
         conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
         cursor = conn.cursor()
 
         # Insert the device data into the database
-        sql = '''INSERT INTO customers (name, mac_address, ipv4_address, ipv6_address) 
+        sql = '''INSERT INTO radacct (username, callingstationid, framedipaddress, framedipv6address) 
                  VALUES (%s, %s, %s, %s)'''
         cursor.execute(sql, (name, mac_address, ipv4_address, ipv6_address))
         conn.commit()
@@ -47,11 +47,11 @@ def devices():
         # Query is searching from database based on name, mac address, ipv4 or ipv6 address
         # It is NOT case sensitive
         # Used Parameterized quieries to protect from SQL injections
-        cursor.execute('''SELECT * FROM customers 
-                       WHERE name LIKE %s or mac_address LIKE %s or ipv4_address LIKE %s or ipv6_address LIKE %s;''', 
+        cursor.execute('''SELECT * FROM radacct 
+                       WHERE username LIKE %s or callingstationid LIKE %s or framedipaddress LIKE %s or framedipv6address LIKE %s;''', 
                        ('%' + query + '%', '%' + query + '%', '%' + query + '%', '%' + query + '%'))
     else:
-        cursor.execute('SELECT * FROM customers;')
+        cursor.execute('SELECT username, callingstationid, framedipaddress, framedipv6address FROM radacct;')
 
     rows = cursor.fetchall()
     cursor.close()
@@ -61,3 +61,4 @@ def devices():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
