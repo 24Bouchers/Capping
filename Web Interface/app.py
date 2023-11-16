b82a86dc9bf6b3a5852296d679ed2c702b1f6179
NickVasquez <nicklas.vasquez1@marist.edu>
2023-11-16 11:55:35 -0500

    Graph

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -12,7 +12,7 @@ def main():
     conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     
-    cursor.execute('SELECT acctstarttime FROM radacct;')
+    cursor.execute('select acctstarttime from radacct order by acctstarttime DESC;')
 
     stime = cursor.fetchall()
     cursor.close()
@@ -24,24 +24,27 @@ def main():
     yesterdayDate = todayDate - timedelta(days = 1)
     todayDate = str(todayDate)
     yesterdayDate = str(yesterdayDate)
-    tempDate = "2023-11-04" # DELETE BEFORE DELIVER!!!!!----------------------------------------------------------------------------------
-    # -------------------------------------------------------------------------------------
-    # MIGHT HAVE TO USE/CONVERT TO UTC TO PREVENT DAYLIGHT SAVINGS ISSUES
-    # --------------------------------------------------------------------------------
+    tempDate = "2023-10-10" # DELETE BEFORE DELIVER!!!!!----------------------------------------------------------------------------------
     # convert current time into minutes
     currentTimeSplit = str(currentTimeUTC).split(':')
+    currentTimeEST = str(currentTimeEST).split(':')
     # counts down to the nearest 15 minute interval
     close15MinInterval =int(currentTimeSplit[1])
+    minIntervalTimeEST = int(currentTimeEST[1])
+    sameTime = True
     while close15MinInterval % 15 != 0:
         close15MinInterval -= 1
+        minIntervalTimeEST -= 1 
+        sameTime = False
     currentTimeMin = close15MinInterval + (int(currentTimeSplit[0])*60)
+
 
     print(currentTimeMin)
     # THESE ARE TEST VARIABLES!!
     # DELETE BEFORE PUSH TO FULL PRODUCTION
     # currentTimeMin = 60
-    yesterdayDate = "2023-11-04"
-
+    yesterdayDate = "2023-10-09"
+    # TODO delete temp code
 
     # list that will store the 15 minute interval numbers
     # going to be every 15 min for 6 hours
@@ -49,12 +52,12 @@ def main():
     hoursInMin = 360 # 360 represents 6 hours
     # loop through the list of times given from acctstarttime
     for x in stime:
-        if x:
-            # convert the acctstarttime into minutes
-            splitDateTime = str(x.get('acctstarttime')).split() # separates date and time into 2 separate list elements
+        # convert the acctstarttime into minutes
+        splitDateTime = str(x.get('acctstarttime')).split() # separates date and time into 2 separate list elements
+        if splitDateTime and splitDateTime[0] != 'None':
             splitTime = str(splitDateTime[1]).split(':')
             totalTimeMin = int(splitTime[1]) + (int(splitTime[0])*60)
-            print(totalTimeMin, splitDateTime[0])
+            # print(totalTimeMin, splitDateTime[0])
             # since the time were comparing against is the closest 15 minute interval that has already passed
             # if a time appears that has todays date and is passed the time were checking from
             # it gets added to the most recent colom of the graph
@@ -84,17 +87,40 @@ def main():
                     if diff <= len(intervals) and diff > -1:
                         intervals[diff+1] += 1
         
-    intervals.reverse()
     # the bottom labels of the line graph representing 15 minute increments
     labels = [''] * 24
-    while close15MinInterval % 15 != 0:
-         close15MinInterval -= 1
+    hourCount = 0
+    startingHour = currentTimeEST[0]
     for x in range(24):
-        if x == 0:
-            labels[x] = currentTimeEST
+        if x == 0 and not sameTime:
+            if int(currentTimeEST[0]) > 12:
+                labels[x] = str(int(currentTimeEST[0])-12) + ':' + currentTimeEST[1] + ' pm'
+            else:
+                labels[x] = currentTimeEST[0] + ':' + currentTimeEST[1] + ' am'
         else:
-             labels[x] = currentTimeEST
-    
+            # subtracts 15 minutes form previous time
+            if int(minIntervalTimeEST) - 15 > 0:
+                minIntervalTimeEST -= 15
+                if int(currentTimeEST[0]) > 12:
+                    labels[x] = str(int(currentTimeEST[0])-12) + ':' + str(minIntervalTimeEST) + ' pm'
+                else:
+                    labels[x] = currentTimeEST[0] + ':' + str(minIntervalTimeEST) + ' am' 
+            # adjusts the hour value
+            else: 
+                if int(currentTimeEST[0]) > 12:
+                    labels[x] = str(int(currentTimeEST[0])-12) + ':00 pm'
+                else:
+                    labels[x] = currentTimeEST[0] + ':00 am'  
+                minIntervalTimeEST =60
+                hourCount += 1
+                currentTimeEST[0] = str(int(startingHour)-hourCount)
+                if currentTimeEST[0] == '0':
+                    currentTimeEST[0] = '12'
+                if int(currentTimeEST[0])  < 0:
+                    currentTimeEST[0] = '23'
+
+    intervals.reverse()
+    labels.reverse()
     return render_template('index.html', labels=labels, values=intervals)
 
 
