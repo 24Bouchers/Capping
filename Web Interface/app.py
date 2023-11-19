4273486105d5d9ba44399c781f701a98715c1ac2
christianb4 <60160063+christianb4@users.noreply.github.com>
2023-11-19 03:58:48 +0000

    fixed logo for flask, logs page

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -178,10 +178,9 @@ def devices():
 
     return render_template('devices.html', rows=rows)
 
-""" logs route draft code
 @app.route('/logs.html', methods=['GET', 'POST'])
 def logs():
-    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='customer_data')
+    conn = pymysql.connect(host='10.10.9.43', user='root', password='', db='radius_netelastic')
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     
     query = request.form.get('query') if request.method == 'POST' else None
@@ -192,14 +191,13 @@ def logs():
                        WHERE time LIKE %s OR username LIKE %s OR reason LIKE %s''',
                        ('%' + query + '%', '%' + query + '%', '%' + query + '%'))
     else:
-        cursor.execute('SELECT LogID, time, username, reason FROM logs;')
+        cursor.execute('SELECT logID, time, username, reason FROM logs;')
 
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
 
     return render_template('logs.html', rows=rows)
-"""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
