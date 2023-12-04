311528b02f0ae9710a0f622019f23a68b5646b37
Liam Haggerty <Liam.Haggerty1@marist.edu>
2023-12-04 09:17:52 -0500

    changed port

--- Web Interface/app.py
+++ Web Interface/app.py
@@ -486,4 +486,4 @@ def logs():
     return render_template('logs.html', rows=rows)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0')
